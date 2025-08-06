7f035ed1154de8efb90cffa6592e7f68f71850f8
Changil.Kim <laterre39@naver.com>
2025-08-06 15:06:13 +0900

    feat: 기본 메뉴 페이지 생성

---
+++ src/app/news/page.tsx
@@ -0,0 +1,11 @@
+export default function News() {
+  return (
+    <main className="mx-auto">
+      <section className="mx-auto max-w-5xl pt-30">
+        <div className="">
+          <span className="text-white">Sorry... | 현재 페이지를 개발하고 있습니다...</span>
+        </div>
+      </section>
+    </main>
+  );
+}
