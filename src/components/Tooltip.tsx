08f115de40362f0c43771757f11b0b3ecc262a1c
Changil.Kim <laterre39@naver.com>
2025-08-07 23:58:36 +0900

    design: 툴팁의 디자인 요소 개선

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -8,7 +8,7 @@ export function Tooltip({ text }: TooltipProps) {
   return (
     <div className={`group relative -translate-y-3`}>
       <MessageCircleWarning size={20}/>
-      <div className="absolute left-full top-1/2 z-10 ml-4 -translate-y-1/2 scale-0 transform rounded-lg bg-white px-3 py-2 text-sm font-medium text-black transition-all duration-300 group-hover:scale-100 whitespace-nowrap">
+      <div className="absolute left-full top-1/2 z-10 ml-4 -translate-y-1/2 opacity-0 pointer-events-none rounded-lg bg-white px-3 py-2 text-sm font-medium text-black transition-opacity duration-300 group-hover:opacity-100 group-hover:pointer-events-auto whitespace-nowrap">
         <div className="absolute -left-1 top-1/2 h-0 w-0 -translate-y-1/2 border-y-4 border-y-transparent border-r-4 border-r-white"></div>
         {text}
       </div>
