5fc4a6c1f4dc184a120a8075fbf49e577904e005
Changil.Kim <laterre39@naver.com>
2025-08-09 01:25:56 +0900

    fix: 툴팁 관련 화면 버그 수정 및 툴팁 기능 개선

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -1,3 +1,4 @@
+import { Tooltip as FlowbiteTooltip } from 'flowbite-react';
 import { MessageCircleWarning } from 'lucide-react';
 
 interface TooltipProps {
@@ -6,12 +7,8 @@ interface TooltipProps {
 
 export function Tooltip({ text }: TooltipProps) {
   return (
-    <div className={`group relative -translate-y-3`}>
-      <MessageCircleWarning size={20}/>
-      <div className="absolute left-full top-1/2 z-10 ml-4 -translate-y-1/2 opacity-0 pointer-events-none rounded-lg bg-white px-3 py-2 text-sm font-medium text-black transition-opacity duration-300 group-hover:opacity-100 group-hover:pointer-events-auto whitespace-nowrap">
-        <div className="absolute -left-1 top-1/2 h-0 w-0 -translate-y-1/2 border-y-4 border-y-transparent border-r-4 border-r-white"></div>
-        {text}
-      </div>
-    </div>
+    <FlowbiteTooltip content={text} placement="right" style="light" className="text-black max-w-60 whitespace-normal">
+      <MessageCircleWarning size={20} />
+    </FlowbiteTooltip>
   );
 }
