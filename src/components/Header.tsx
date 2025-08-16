332371f07a649814b52a285020c23da4fbc36548
Changil.Kim <laterre39@naver.com>
2025-08-16 15:55:23 +0900

    feat: 이벤트 페이지 기본 생성

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,172 +1,182 @@
 "use client";
 
 import Link from 'next/link';
-import { useState, useEffect } from 'react';
+import {useEffect, useState} from 'react';
 
 export function Header() {
-  const [isScrolled, setIsScrolled] = useState(false);
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [isScrolled, setIsScrolled] = useState(false);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      setIsScrolled(window.scrollY > 10);
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsScrolled(window.scrollY > 10);
+        };
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
     };
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
 
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-
-  return (
-    <header
-      className={`sticky top-0 z-50 w-full transition-all duration-300 mb-6 ${
-        isScrolled
-          ? 'border-b border-gray-200/50 bg-white/80 shadow-md backdrop-blur-sm'
-          : 'bg-transparent'
-      } relative`}
-    >
-      <div className="container mx-auto flex h-20 max-w-5xl items-center justify-between px-4">
-        <Link href="/" className="flex items-center space-x-2">
+    return (
+        <header
+            className={`sticky top-0 z-50 w-full transition-all duration-300 mb-6 ${
+                isScrolled
+                    ? 'border-b border-gray-200/50 bg-white/80 shadow-md backdrop-blur-sm'
+                    : 'bg-transparent'
+            } relative`}
+        >
+            <div className="container mx-auto flex h-20 max-w-5xl items-center justify-between px-4">
+                <Link href="/" className="flex items-center space-x-2">
           <span
-            className={`font-light text-3xl transition-all duration-300 underline decoration-4 decoration-[#39C5BB] ${
-              isScrolled ? 'text-black' : 'text-white'
-            }`}
+              className={`font-light text-3xl transition-all duration-300 underline decoration-4 decoration-[#39C5BB] ${
+                  isScrolled ? 'text-black' : 'text-white'
+              }`}
           >
             HATSUNEMIKU.ME
           </span>
-        </Link>
-        <nav className="hidden md:flex">
-          <ul className="flex items-center space-x-8 text-base">
-            <li>
-              <Link
-                href="/about"
-                className={`font-medium transition-colors ${
-                  isScrolled ? 'text-black' : 'text-white'
-                }`}
-              >
-                About
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/music"
-                className={`font-medium transition-colors ${
-                  isScrolled ? 'text-black' : 'text-white'
-                }`}
-              >
-                Music
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/gallery"
-                className={`font-medium transition-colors ${
-                  isScrolled ? 'text-black' : 'text-white'
-                }`}
-              >
-                Gallery
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/news"
-                className={`font-medium transition-colors ${
-                  isScrolled ? 'text-black' : 'text-white'
-                }`}
-              >
-                News
-              </Link>
-            </li>
-          </ul>
-        </nav>
-        <div className="md:hidden">
-          <button
-            onClick={toggleMenu}
-            className={`transition-colors ${
-              isScrolled ? 'text-black' : 'text-white'
-            }`}
-          >
-            <svg
-              className="h-6 w-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d={isMenuOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h10M4 12h10m-7 6h4"}
-              ></path>
-            </svg>
-          </button>
-        </div>
-      </div>
-      {isMenuOpen && (
-        <nav
-          className={`absolute w-full md:hidden ${
-            isScrolled ? 'bg-white/80 backdrop-blur-sm' : 'bg-black/50 backdrop-blur-sm'
-          }`}
-        >
-          <ul
-            className={`flex flex-col items-center divide-y ${
-              isScrolled ? 'divide-gray-200/50' : 'divide-white/20'
-            }`}
-          >
-            <li className="w-full text-center">
-              <Link
-                href="/about"
-                className={`block py-4 font-medium transition-colors ${
-                  isScrolled
-                    ? 'text-dark' : 'text-white'
-                }`}
-                onClick={() => setIsMenuOpen(false)}
-              >
-                About
-              </Link>
-            </li>
-            <li className="w-full text-center">
-              <Link
-                href="/music"
-                className={`block py-4 font-medium transition-colors ${
-                  isScrolled
-                    ? 'text-dark' : 'text-white'
-                }`}
-                onClick={() => setIsMenuOpen(false)}
-              >
-                Music
-              </Link>
-            </li>
-            <li className="w-full text-center">
-              <Link
-                href="/gallery"
-                className={`block py-4 font-medium transition-colors ${
-                  isScrolled
-                    ? 'text-dark' : 'text-white'
-                }`}
-                onClick={() => setIsMenuOpen(false)}
-              >
-                Gallery
-              </Link>
-            </li>
-            <li className="w-full text-center">
-              <Link
-                href="/news"
-                className={`block py-4 font-medium transition-colors ${
-                  isScrolled
-                    ? 'text-dark' : 'text-white'
-                }`}
-                onClick={() => setIsMenuOpen(false)}
-              >
-                News
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      )}
-    </header>
-  );
+                </Link>
+                <nav className="hidden md:flex">
+                    <ul className="flex items-center space-x-8 text-base">
+                        <li>
+                            <Link
+                                href="/about"
+                                className={`font-medium transition-colors ${
+                                    isScrolled ? 'text-black' : 'text-white'
+                                }`}
+                            >
+                                About
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href="/music"
+                                className={`font-medium transition-colors ${
+                                    isScrolled ? 'text-black' : 'text-white'
+                                }`}
+                            >
+                                Music
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href="/event"
+                                className={`font-medium transition-colors ${
+                                    isScrolled ? 'text-black' : 'text-white'
+                                }`}
+                            >
+                                Event
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href="/gallery"
+                                className={`font-medium transition-colors ${
+                                    isScrolled ? 'text-black' : 'text-white'
+                                }`}
+                            >
+                                Gallery
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href="/news"
+                                className={`font-medium transition-colors ${
+                                    isScrolled ? 'text-black' : 'text-white'
+                                }`}
+                            >
+                                News
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+                <div className="md:hidden">
+                    <button
+                        onClick={toggleMenu}
+                        className={`transition-colors ${
+                            isScrolled ? 'text-black' : 'text-white'
+                        }`}
+                    >
+                        <svg
+                            className="h-6 w-6"
+                            fill="none"
+                            stroke="currentColor"
+                            viewBox="0 0 24 24"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d={isMenuOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h10M4 12h10m-7 6h4"}
+                            ></path>
+                        </svg>
+                    </button>
+                </div>
+            </div>
+            {isMenuOpen && (
+                <nav
+                    className={`absolute w-full md:hidden ${
+                        isScrolled ? 'bg-white/80 backdrop-blur-sm' : 'bg-black/50 backdrop-blur-sm'
+                    }`}
+                >
+                    <ul
+                        className={`flex flex-col items-center divide-y ${
+                            isScrolled ? 'divide-gray-200/50' : 'divide-white/20'
+                        }`}
+                    >
+                        <li className="w-full text-center">
+                            <Link
+                                href="/about"
+                                className={`block py-4 font-medium transition-colors ${
+                                    isScrolled
+                                        ? 'text-dark' : 'text-white'
+                                }`}
+                                onClick={() => setIsMenuOpen(false)}
+                            >
+                                About
+                            </Link>
+                        </li>
+                        <li className="w-full text-center">
+                            <Link
+                                href="/music"
+                                className={`block py-4 font-medium transition-colors ${
+                                    isScrolled
+                                        ? 'text-dark' : 'text-white'
+                                }`}
+                                onClick={() => setIsMenuOpen(false)}
+                            >
+                                Music
+                            </Link>
+                        </li>
+                        <li className="w-full text-center">
+                            <Link
+                                href="/gallery"
+                                className={`block py-4 font-medium transition-colors ${
+                                    isScrolled
+                                        ? 'text-dark' : 'text-white'
+                                }`}
+                                onClick={() => setIsMenuOpen(false)}
+                            >
+                                Gallery
+                            </Link>
+                        </li>
+                        <li className="w-full text-center">
+                            <Link
+                                href="/news"
+                                className={`block py-4 font-medium transition-colors ${
+                                    isScrolled
+                                        ? 'text-dark' : 'text-white'
+                                }`}
+                                onClick={() => setIsMenuOpen(false)}
+                            >
+                                News
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+            )}
+        </header>
+    );
 }
