f17b3e0224faec58f078fa9670201a45356d7498
Changil.Kim <laterre39@naver.com>
2025-08-21 01:18:04 +0900

    chore(schedule): 요청한 스케줄 추가

--- src/data/vocaloidEventLists.ts
+++ src/data/vocaloidEventLists.ts
@@ -1,9 +1,27 @@
 export const vocaloidEventLists = [
     {
+        eventStartDate: "2025.02.11",
+        eventEndDate: "2025.02.11",
+        eventName: "Hatsune Miku Symphony 2025 ~10th Anniversary~ in Sapporo",
+        eventSite: "https://sp.wmg.jp/mikusymphony/10th/",
+    },
+    {
+        eventStartDate: "2025.04.12",
+        eventEndDate: "2025.04.12",
+        eventName: "Hatsune Miku Symphony 2025 ~10th Anniversary~ in Osaka",
+        eventSite: "https://sp.wmg.jp/mikusymphony/10th/",
+    },
+    {
         eventStartDate: "2025.04.26",
         eventEndDate: "2025.04.27",
         eventName: "Nico Nico Chokaigi 2025",
         eventSite: "https://chokaigi.jp/",
+    },
+    {
+        eventStartDate: "2025.07.06",
+        eventEndDate: "2025.07.06",
+        eventName: "Hatsune Miku Symphony 2025 ~10th Anniversary~ in Tokyo",
+        eventSite: "https://sp.wmg.jp/mikusymphony/10th/",
     },
     {
         eventStartDate: "2025.08.01",
@@ -24,9 +42,21 @@ export const vocaloidEventLists = [
         eventSite: "https://magicalmirai.com/2025/tokyo_map_en.html",
     },
     {
+        eventStartDate: "2025.10.04",
+        eventEndDate: "2025.10.04",
+        eventName: "Hatsune Miku Symphony 2025 ~10th Anniversary~ in Yokohama",
+        eventSite: "https://sp.wmg.jp/mikusymphony/10th/",
+    },
+    {
         eventStartDate: "2025.11.29",
         eventEndDate: "2025.11.30",
         eventName: "HATSUNE MIKU EXPO 2025 in Seoul",
         eventSite: "https://ticket.melon.com/performance/index.htm?prodId=211664",
+    },
+    {
+        eventStartDate: "2025.12.27",
+        eventEndDate: "2025.12.27",
+        eventName: "Hatsune Miku Symphony 2025 ~10th Anniversary~ in Kobe",
+        eventSite: "https://sp.wmg.jp/mikusymphony/10th/",
     },
 ];
