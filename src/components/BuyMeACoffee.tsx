f82e5a4dff4bf7417e660ea98ae396ba84db76ab
Changil.Kim <laterre39@naver.com>
2025-09-03 01:46:42 +0900

    design(main): BuyMeaCoffee 버튼 디자인 개선

--- src/components/BuyMeACoffee.tsx
+++ src/components/BuyMeACoffee.tsx
@@ -5,20 +5,17 @@ import {Coffee} from "lucide-react";
 
 export function BuyMeACoffee() {
     return (
-        <div className="group fixed bottom-5 left-5 z-50">
-            <Link
-                href="https://www.buymeacoffee.com/laterre39"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="flex h-14 w-14 items-center justify-center rounded-full bg-[#39C5BB] text-white shadow-lg backdrop-blur-sm transition-all duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:shadow-xl"
-            >
-                <Coffee className="h-7 w-7"/>
-            </Link>
-            <div
-                className="absolute left-full top-1/2 ml-4 -translate-y-1/2 scale-0 rounded-lg bg-[#39C5BB] px-3 py-2 text-sm font-semibold text-white opacity-0 transition-all duration-300 ease-in-out group-hover:scale-100 group-hover:opacity-100 whitespace-nowrap"
-            >
-                미쿠에게 커피 사주기!
-            </div>
-        </div>
+        <Link
+            href="https://www.buymeacoffee.com/mikumiku"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="group fixed bottom-5 left-5 z-50 flex h-14 items-center justify-center gap-2 rounded-full bg-[#39C5BB] px-4 text-gray-900 shadow-lg transition-all duration-300 ease-in-out hover:w-48"
+        >
+            <Coffee className="h-7 w-7 flex-shrink-0"/>
+            <p
+                className="max-w-0 overflow-hidden whitespace-nowrap text-sm transition-all duration-300 ease-in-out group-hover:max-w-full">
+                <span className="font-bold">개발자</span> 커피 사주기 <br/> <span className="font-bold">미쿠</span>도 <span className="font-bold">좋아해요</span>
+            </p>
+        </Link>
     );
 }
