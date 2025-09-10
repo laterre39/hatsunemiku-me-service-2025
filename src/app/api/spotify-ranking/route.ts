d408b109f1d2427e37bf1d69bb95f684fe748359
Changil.Kim <laterre39@naver.com>
2025-09-10 18:55:02 +0900

    feat(api): 유튜브 및 스포티파이 캐싱 시간 12시간으로 변경

--- src/app/api/spotify-ranking/route.ts
+++ src/app/api/spotify-ranking/route.ts
@@ -2,8 +2,8 @@
 
 import {NextResponse} from 'next/server';
 
-// Revalidate at most every 3 hours (3 * 60 * 60 = 10800 seconds)
-export const revalidate = 21600;
+// Revalidate at most every 12 hours (12 * 60 * 60 = 43200 seconds)
+export const revalidate = 43200;
 
 // 스포티파이 API 액세스 토큰을 가져오는 함수
 async function getAccessToken() {
