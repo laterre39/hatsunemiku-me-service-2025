dd1b838fc961459a6b9400e5281f8906b13dcffd
laterre39 <laterre39@naver.com>
2025-11-14 05:35:41 +0900

    feat: 소개 페이지 수정

--- src/data/specialThanksList.ts
+++ src/data/specialThanksList.ts
@@ -1,16 +1,26 @@
 export const specialThanksList = [
-    {
-        name: '하츠네 미쿠 마이너 갤러리',
-        contribution: '아이디어 및 정보 제공',
-        links: [
-            {type: 'default', url: 'https://gall.dcinside.com/mgallery/board/lists?id=mikuhatsune'}
-        ]
-    },
-    {
-        name: '자택관리인',
-        contribution: '일러스트 제공',
-        links: [
-            {type: 'default', url: 'https://gall.dcinside.com/mgallery/board/view?id=mikuhatsune&no=381515'},
-        ]
-    },
+  {
+    name: '후원 서포터',
+    contribution: '사이트 후원, 지원 감사합니다!',
+    links: [
+      {type: 'default', url: 'https://buymeacoffee.com/mikumiku'}
+    ]
+  },
+  {
+    name: '하츠네 미쿠 마이너 갤러리',
+    contribution: '아이디어 및 정보 제공',
+    links: [
+      {type: 'default', url: 'https://gall.dcinside.com/mgallery/board/lists?id=mikuhatsune'}
+    ]
+  },
+  {
+    name: '자택관리인',
+    contribution: '일러스트 제공',
+    links: [
+      {
+        type: 'default',
+        url: 'https://gall.dcinside.com/mgallery/board/view?id=mikuhatsune&no=381515'
+      },
+    ]
+  },
 ];
