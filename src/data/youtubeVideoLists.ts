97907c0a640bd191d345b830f2f6820d72590e5d
laterre39 <laterre39@naver.com>
2025-11-13 14:24:56 +0900

    feat: 베스트 뮤비 섹션의 유튜브 링크 변경

--- src/data/youtubeVideoLists.ts
+++ src/data/youtubeVideoLists.ts
@@ -1,15 +1,8 @@
 export const youtubeVideoLists: string[] = [
-  '3iUgKH8c7p4', // いますぐ輪廻 （Retry Now） / 하츠네 미쿠
-  'BI9Ue6JwJic', // DECO*27 - チェリーポップ feat. 初音ミク
-  'vg6pnvn1u10', // 장난기 기능(おちゃめ機能) / 카사네 테토
-  'xdNIO7WudEw', // TAK - ‘고독사이코 (Psycho Mode)
-  'wpEtAg_ngOU', // 순하추등 / 세카이
-  'Bi0FB2sBCjQ', // 【猫使ビィ＆初音ミク】グリッチギツネ【オリジナル曲】
-  'Ig-a4aJR3jM', // 8.32 (Acoustic Ver.) / *Luna feat.Hatsune Miku
-  '6Fm-wW_57FY', // MV「死別」 / Shannon feat. GUMI
-  'JW3N-HvU0MA', // 미키토P 『소녀 레이』 MV
-  'TA5OFS_xX0c', // 샤를 / flower
-  'oRJBwaZ59fQ', // ベノム／かいりきベア feat.flower
-  'GXoFGszHVeU', // でていけー!!!! - cosMo＠暴走P feat.音街ウナ
-  '9O2VyUM5MlQ', // まにまに / 初音ミク
+  'QQdddUKUkko', // アガリ症 / 重音テトSV
+  'uWMr16O_Aso', // 目撃！テト31世🎉🎃🧟雨衣・重音テト
+  'PTEF6pnP_FU', // 雲隠 / なみぐる feat.双葉 湊音
+  'gHHAslijFHI', // 黒塗り世界宛て書簡 (DJ WILDPARTY XXL edit)
+  'pR1PKuHKu3s', // 蓮華 ver.2 feat.東北きりたん＆重音テト　　by ____natural
+  'NGgfBgssCvY', // 土に還るその日まで (Peg Remix)
 ];
