cef92fe4d25ae441eee910b931cd5281d533e22c
laterre39 <laterre39@naver.com>
2025-11-14 16:38:11 +0900

    feat: 보컬로이드 노래 데이터 추가

--- src/data/youtubeVideoLists.ts
+++ src/data/youtubeVideoLists.ts
@@ -5,4 +5,11 @@ export const youtubeVideoLists: string[] = [
   'gHHAslijFHI', // 黒塗り世界宛て書簡 (DJ WILDPARTY XXL edit)
   'pR1PKuHKu3s', // 蓮華 ver.2 feat.東北きりたん＆重音テト　　by ____natural
   'NGgfBgssCvY', // 土に還るその日まで (Peg Remix)
+  'XSLhsjepelI', // 그린라이츠・세레나데
+  '7r8DfN6g4NY', // とっておキューティー
+  'fFL9NeOF7XM', // 행인dope(두부) / 나미구루 feat.카후(可不)
+  '_FZw8sj-LwQ', // ミューズ (feat. 初音ミク)
+  'Gz1rRgcRNfw', // flower 『미저리』 오리지널 MV/루완
+  'fi-5aSGR024', // 裏命 - わたしレプリカ (RIME - I Replica)
+  'MFs7FgQU2xM', // スライ村 / ついなちゃん
 ];
