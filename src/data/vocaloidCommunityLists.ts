574f530eeab8adc5bc7c9c7c397f1d1634d1da74
laterre39 <laterre39@naver.com>
2025-11-20 15:32:45 +0900

    feat: 보컬로이드 커뮤니티 데이터 수정

--- src/data/vocaloidCommunityLists.ts
+++ src/data/vocaloidCommunityLists.ts
@@ -5,14 +5,19 @@ export const vocaloidCommunityLists = [
     url: "https://gall.dcinside.com/mgallery/board/lists/?id=mikuhatsune",
   },
   {
+    name: "하츠네 미쿠 채널",
+    description: "아직 보지 않은 미래에서, 처음의 소리가 찾아오다.",
+    url: "https://arca.live/b/hatsunemiku",
+  },
+  {
     name: "시유 마이너 갤러리",
     description: "대한민국 보컬로이드 시유(SeeU)의 팬 커뮤니티입니다.",
     url: "https://gall.dcinside.com/mgallery/board/lists?id=seeu",
   },
   {
-    name: "하츠네 미쿠 채널",
-    description: "아직 보지 않은 미래에서, 처음의 소리가 찾아오다.",
-    url: "https://arca.live/b/hatsunemiku",
+    name: "보컬로이드 제국",
+    description: "보컬로이드와 관련된 다양한 작품과 새로운 소식을 만나보세요.",
+    url: "https://cafe.naver.com/vocaloidempire",
   },
   {
     name: "보컬로이드 채널",
@@ -23,35 +28,5 @@ export const vocaloidCommunityLists = [
     name: "보카로 차트",
     description: "보컬로이드 곡 랭킹 제공",
     url: "https://vocaloard.injpok.tokyo/",
-  },
-  {
-    name: "보카로 가사 위키",
-    description: "보컬로이드 노래의 가사와 번역을 제공하는 위키입니다.",
-    url: "https://vocaloidlyrics.fandom.com/wiki/VOCALOID_Lyrics_Wiki",
-  },
-  {
-    name: "미쿠 엑스포 정보 사이트",
-    description: "하츠네 미쿠 엑스포 관련 정보를 제공하고 있습니다.",
-    url: "https://mm-info.miku.kr/",
-  },
-  {
-    name: "K!!te",
-    description: "몰랐던 취향의 악곡을 들어보자!",
-    url: "https://kiite.jp/",
-  },
-  {
-    name: "Vocabirth",
-    description: "특정한 날에 올라온 보카로 곡 찾아보기!",
-    url: "https://vocabirth.netlify.app/",
-  },
-  {
-    name: "Vocalendar",
-    description: "보컬로이드 정보를 한눈에",
-    url: "https://vocalendar.jp/",
-  },
-  {
-    name: "VocaDB",
-    description: "보컬로이드 데이터베이스",
-    url: "https://vocadb.net/",
   },
 ];
