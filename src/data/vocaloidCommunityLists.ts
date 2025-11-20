384ba8e9b8f72be3a3064d7619f8e171e9b194f8
laterre39 <laterre39@naver.com>
2025-11-21 04:15:33 +0900

    feat: 보컬로이드 커뮤니티 데이터 수정

--- src/data/vocaloidCommunityLists.ts
+++ src/data/vocaloidCommunityLists.ts
@@ -5,19 +5,14 @@ export const vocaloidCommunityLists = [
     url: "https://gall.dcinside.com/mgallery/board/lists/?id=mikuhatsune",
   },
   {
-    name: "하츠네 미쿠 채널",
-    description: "아직 보지 않은 미래에서, 처음의 소리가 찾아오다.",
-    url: "https://arca.live/b/hatsunemiku",
-  },
-  {
     name: "시유 마이너 갤러리",
     description: "대한민국 보컬로이드 시유(SeeU)의 팬 커뮤니티입니다.",
     url: "https://gall.dcinside.com/mgallery/board/lists?id=seeu",
   },
   {
-    name: "보컬로이드 제국",
-    description: "보컬로이드와 관련된 다양한 작품과 새로운 소식을 만나보세요.",
-    url: "https://cafe.naver.com/vocaloidempire",
+    name: "하츠네 미쿠 채널",
+    description: "아직 보지 않은 미래에서, 처음의 소리가 찾아오다.",
+    url: "https://arca.live/b/hatsunemiku",
   },
   {
     name: "보컬로이드 채널",
@@ -25,8 +20,18 @@ export const vocaloidCommunityLists = [
     url: "https://arca.live/b/vocalo",
   },
   {
-    name: "보카로 차트",
-    description: "보컬로이드 곡 랭킹 제공",
-    url: "https://vocaloard.injpok.tokyo/",
+    name: "보컬로이드 제국",
+    description: "보컬로이드와 관련된 다양한 작품과 새로운 소식을 만나보세요.",
+    url: "https://cafe.naver.com/vocaloidempire",
+  },
+  {
+    name: "Vocaloid Amino",
+    description: "Vocaloid fans rejoice! The largest mobile community for VOCALOID and UTAU!",
+    url: "https://aminoapps.com/c/vocaloid/home/",
+  },
+  {
+    name: "VocaVerse Network",
+    description: "VVN",
+    url: "https://vocaverse.network/",
   },
 ];
