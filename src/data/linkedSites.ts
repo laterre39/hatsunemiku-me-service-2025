a1bbf10c1914f09f003e186aab7d8a30ca98c9a7
laterre39 <laterre39@naver.com>
2025-11-21 05:04:26 +0900

    fix: 잘못 수정된 주소 수정 및 정리

--- src/data/linkedSites.ts
+++ src/data/linkedSites.ts
@@ -7,24 +7,12 @@ interface LinkedSite {
 export const linkedSites: LinkedSite[] = [
   {name: "Official Blog", url: "https://blog.piapro.net/", showInFooter: true},
   {name: "Official X", url: "https://x.com/cfm_miku_en", showInFooter: true},
-  {
-    name: "Official Facebook",
-    url: "https://www.facebook.com/HatsuneMikuOfficialPage",
-    showInFooter: true
-  },
-  {
-    name: "Official Instagram",
-    url: "https://www.instagram.com/cfm_mikustagram/",
-    showInFooter: true
-  },
+  {name: "Official Facebook", url: "https://www.facebook.com/HatsuneMikuOfficialPage", showInFooter: true},
+  {name: "Official Instagram", url: "https://www.instagram.com/cfm_mikustagram/", showInFooter: true},
   {name: "KARENT Music", url: "https://karent.jp/", showInFooter: true},
   {name: "K!!te", url: "https://kiite.jp/", showInFooter: true},
   {name: "마지미라 정보", url: "https://mm-info.miku.kr/", showInFooter: false},
-  {
-    name: "보카로 가사 위키",
-    url: "https://vocaloidlyrics.fandom.com/wiki/VOCALOID_Lyrics_Wiki",
-    showInFooter: false
-  },
+  {name: "보카로 가사 위키", url: "http://vocaro.wikidot.com/", showInFooter: false},
   {name: "Vocaloid collection", url: "https://vocaloid-collection.jp/", showInFooter: false},
   {name: "Vocaloard Charts", url: "https://vocaloard.injpok.tokyo/", showInFooter: false},
   {name: "Piapro", url: "https://piapro.jp/", showInFooter: false},
