ecd2c96f7785d3f562796041a18548c0044b149a
laterre39 <laterre39@naver.com>
2025-11-26 19:27:39 +0900

    fix: HelmetProvider 누락 오류 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import "./globals.css";
-import { Header } from "@/components/Header";
-import { Footer } from "@/components/Footer";
+import {Header} from "@/components/Header";
+import {Footer} from "@/components/Footer";
 import localFont from 'next/font/local'
-import { FlowbiteClient } from '@/components/FlowbiteClient';
-import { ScrollToTopButton } from '@/components/ScrollToTopButton';
-import { BuyMeACoffee } from "@/components/BuyMeACoffee";
-import { Helmet } from "@dr.pogodin/react-helmet";
+import {FlowbiteClient} from '@/components/FlowbiteClient';
+import {ScrollToTopButton} from '@/components/ScrollToTopButton';
+import {BuyMeACoffee} from "@/components/BuyMeACoffee";
+import {Helmet, HelmetProvider} from "@dr.pogodin/react-helmet";
 
 const pretendard = localFont({
   src: '../assets/fonts/PretendardVariable.woff2',
@@ -17,8 +17,8 @@ const pretendard = localFont({
 })
 
 export default function RootLayout({
-  children,
-}: Readonly<{
+                                     children,
+                                   }: Readonly<{
   children: React.ReactNode;
 }>) {
   const siteUrl = "https://hatsunemiku.me/";
@@ -27,45 +27,47 @@ export default function RootLayout({
   const imageUrl = `${siteUrl}cherrypop.png`;
 
   return (
-    <html lang="ko" className={`${pretendard.variable} scroll-smooth`}>
-      <body>
+      <HelmetProvider>
+        <html lang="ko" className={`${pretendard.variable} scroll-smooth`}>
+        <body>
         <Helmet>
           <title>{title}</title>
-          <link rel="canonical" href="https://hatsunemiku.me/" />
-          <meta name="description" content={description} />
-          <meta charSet="utf-8" />
-          <link rel="icon" href="/cherrypop_ico.png" />
-          
+          <link rel="canonical" href="https://hatsunemiku.me/"/>
+          <meta name="description" content={description}/>
+          <meta charSet="utf-8"/>
+          <link rel="icon" href="/cherrypop_ico.png"/>
+
           {/* Open Graph / Facebook */}
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content={siteUrl} />
-          <meta property="og:title" content={title} />
-          <meta property="og:description" content={description} />
-          <meta property="og:image" content={imageUrl} />
-          <meta property="og:image:width" content="800" />
-          <meta property="og:image:height" content="600" />
-          <meta property="og:site_name" content={title} />
-          <meta property="og:locale" content="ko_KR" />
+          <meta property="og:type" content="website"/>
+          <meta property="og:url" content={siteUrl}/>
+          <meta property="og:title" content={title}/>
+          <meta property="og:description" content={description}/>
+          <meta property="og:image" content={imageUrl}/>
+          <meta property="og:image:width" content="800"/>
+          <meta property="og:image:height" content="600"/>
+          <meta property="og:site_name" content={title}/>
+          <meta property="og:locale" content="ko_KR"/>
 
           {/* Twitter */}
-          <meta property="twitter:card" content="summary_large_image" />
-          <meta property="twitter:url" content={siteUrl} />
-          <meta property="twitter:title" content={title} />
-          <meta property="twitter:description" content={description} />
-          <meta property="twitter:image" content={imageUrl} />
+          <meta property="twitter:card" content="summary_large_image"/>
+          <meta property="twitter:url" content={siteUrl}/>
+          <meta property="twitter:title" content={title}/>
+          <meta property="twitter:description" content={description}/>
+          <meta property="twitter:image" content={imageUrl}/>
         </Helmet>
-        
+
         <div className="relative flex min-h-screen flex-col bg-[url('/main_bg.png')] bg-repeat">
-          <Header />
+          <Header/>
           <main className="flex-1 container mx-auto px-4 max-w-5xl">
             {children}
           </main>
-          <Footer />
-          <FlowbiteClient />
-          <ScrollToTopButton />
-          <BuyMeACoffee />
+          <Footer/>
+          <FlowbiteClient/>
+          <ScrollToTopButton/>
+          <BuyMeACoffee/>
         </div>
-      </body>
-    </html>
+        </body>
+        </html>
+      </HelmetProvider>
   );
 }
