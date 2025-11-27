e84dffd2598dc47dfcd26580dd4366c3dbd3bd89
laterre39 <laterre39@naver.com>
2025-11-27 14:53:37 +0900

    feat: 이벤트 페이지 메타데이터 적용

--- src/app/event/page.tsx
+++ src/app/event/page.tsx
@@ -1,4 +1,15 @@
 import { EventPageClient } from '@/components/EventPageClient';
+import type {Metadata} from "next";
+
+export const metadata: Metadata = {
+  title: 'Event',
+  description: '하츠네 미쿠와 보컬로이드 관련 최신 이벤트 정보를 확인하세요!',
+  openGraph: {
+    title: 'Event | HATSUNEMIKU.ME',
+    description: '하츠네 미쿠와 보컬로이드 관련 최신 이벤트 정보를 확인하세요!',
+    url: 'https://hatsunemiku.me/event',
+  },
+};
 
 export default function EventPage() {
   return <EventPageClient />; // TODO:추후에 리팩토링
