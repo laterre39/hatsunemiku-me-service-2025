e0ae2d260d2fdbc4dbb707c0a4c64acb4bbf8865
laterre39 <laterre39@naver.com>
2025-12-02 06:12:56 +0900

    chore: 이미지 서버 주소 추가

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '**',
       },
+      {
+        protocol: 'https',
+        hostname: 'nicovideo.cdn.nimg.jp',
+        port: '',
+        pathname: '**',
+      },
     ],
   },
   /* config options here */
