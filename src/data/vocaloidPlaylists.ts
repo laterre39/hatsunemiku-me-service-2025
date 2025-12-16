1e28f66ba47103928913f85bd17bcc7284e64b99
laterre39 <laterre39@naver.com>
2025-12-16 13:55:12 +0900

    chore: 플리 데이터 추가및 수정

--- src/data/vocaloidPlaylists.ts
+++ src/data/vocaloidPlaylists.ts
@@ -10,21 +10,13 @@ export interface PlaylistInfo {
 
 export const vocaloidPlaylists: PlaylistInfo[] = [
   {
-    id: 'PLwtkfpqgkPlNcHB-QUlXOGTtQaXLJ_txr',
+    id: 'PL4DYOoQTb3ibYGtVQ26U28ZC0d1rxaKzC',
     platform: 'youtube', // youtube, spotify 플랫폼을 지정합니다.
-    title: '에모이 도핑제',
-    description: '듣고 가슴벅차고 에모이해지는 곡들을 모았습니다. (제작자의 취향이 많이 반영되어 있습니다.)',
-    creator: '카피포리',
+    title: 'MIKU EXPO 2025 ASIA in Seoul',
+    description: '한국에서 개최된 미쿠 엑스포 인 서울의 감동을 지금 느꺄보세요!',
+    creator: 'MIKUMIKU',
     //featuredTrackIndices: [0, 2, 4, 6, 8], // 섬네일에 표시될 음악을 5개 정의합니다.
     isSlider: true, // 추천 플리에 보일지 여부
-  },
-  {
-    id: 'PLRs0SDrpFZ7tru4sztL1bQym2J-QZR_kP',
-    platform: 'youtube',
-    title: '일렉트로닉 하이',
-    description: '전자음으로 널 유린하겠다',
-    creator: 'ఠ_ఠ',
-    isSlider: true,
   },
   {
     id: 'PL4DYOoQTb3iY-jliX1Ta_sCwpz02esiZz',
@@ -33,6 +25,22 @@ export const vocaloidPlaylists: PlaylistInfo[] = [
     description: '보컬로이드도 신토불이! 한국의 프로듀서들을 만나보세요.',
     creator: 'MIKUMIKU 외 2명',
     isSlider: true,
+  },
+  {
+    id: 'PLwtkfpqgkPlNcHB-QUlXOGTtQaXLJ_txr',
+    platform: 'youtube',
+    title: '에모이 도핑제',
+    description: '듣고 가슴벅차고 에모이해지는 곡들을 모았습니다. (제작자의 취향이 많이 반영되어 있습니다.)',
+    creator: '카피포리',
+    isSlider: true,
+  },
+  {
+    id: 'PLRs0SDrpFZ7tru4sztL1bQym2J-QZR_kP',
+    platform: 'youtube',
+    title: '일렉트로닉 하이',
+    description: '전자음으로 널 유린하겠다',
+    creator: 'ఠ_ఠ',
+    isSlider: false,
   },
   {
     id: '0rDPDKkz0Rf36ayZVodsUj?si=8o_UzidRQMuK8t9FiQw7hg',
@@ -51,7 +59,7 @@ export const vocaloidPlaylists: PlaylistInfo[] = [
     isSlider: false,
   },
   {
-    id: 'PLRs0SDrpFZ7sDAxpc_UqAGZzxxvjm5TPn&si=w-AYBeMHZjgWo0CE',
+    id: 'PLRs0SDrpFZ7sDAxpc_UqAGZzxxvjm5TPn',
     platform: 'youtube',
     title: '간뇌 플레이리스트',
     description: '감동적인곡을들으면간뇌가덜덜떨리고간뇌가덜덜떨리면간뇌의자극에의해시상하부의자극에의해눈물샘이자극되어눈물이줄줄나오게됩니다',
